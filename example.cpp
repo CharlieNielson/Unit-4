0237789c939c7963c1a89e5a9006d4174eea2561
CharlieNielson <charlienielson@mail.weber.edu>
2024-09-25 13:37:39 -0600

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// A class is an object you can define, and a method is a function that is part of a class.
+
+class example {
+    public:
+    // Actions go here.
+    example();// Constructor
+    private:
+    // Properties go here.
+}
+
+example::example() {/*scope resolution operator is ::*/
+    // Property definitions.
+}
