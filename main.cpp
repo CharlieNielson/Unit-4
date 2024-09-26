cf252b7f16080f97fe218fa7beb51c4f7acfcd94
CharlieNielson <charlienielson@mail.weber.edu>
2024-09-26 16:57:35 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,30 @@ using namespace std;
 
 
 int main() {
- srand(time(NULL));  //Set the random seed
- //Declare die
- Die d1(6);
- //Call methods
- d1.roll();
- d1.display();
+    srand(time(NULL));  //Set the random seed
+    //Declare die
+    int sides = 6;
+    if (true) {
+        Die d1(1);
+        d1.roll();
+        d1.display();
+    }
+    Die d1(6);
+    d1.roll();
+    d1.display();
+    if (d1.getDieValue() == 1) {
+        cout << "Doubles!\n";
+    }
+    while (sides > 0) {
+        cout << "How many sides for your dice(0 for quit): ";
+        cin >> sides;
+        cout << endl;
+        if (sides != 0) {
+            Die d1(sides);
+            //Call methods
+            d1.roll();
+            d1.display();
+        }
+    }
 }
 
