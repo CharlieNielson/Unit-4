fb61860ac832461ef81f0f637d28d710badcc844
CharlieNielson <charlienielson@mail.weber.edu>
2024-10-14 09:13:34 -0600

    Update PIG.h

--- PIG.h
+++ PIG.h
@@ -2,27 +2,54 @@
 #include <iostream>
 using namespace std;
 
-class PIG{
+class play {
     public:
-    PIG();
-    void roll() {
-       dieValue = rand() % 6 + 1;
-    }
-    int stop(int);
-    int getValueDie() {
-        return dieValue;
-    }
-    int getTurnTotal(int);
-    int getTotal(int);
+        play();
+        void roll(bool);
+        void stop(bool);
+        int getDieValue() { return dieValue; }
+        int getTurnTotal() { return turnTotal; }
+        int getTotal() { return gameTotal; }
+        void startTurn() { dieValue = 6; }
 
     private:
-      int dieValue;
-      int turnNUM;
-      int gameTotal;
+        int dieValue;
+        int turnTotal;
+        int gameTotal;
 };
 
-PIG::PIG() {
-    srand(time(NULL));  //Set the random seed
+play::play() {
     dieValue = rand() % 6 + 1;
-    cout << "Thing: " << dieValue << endl;
+    turnTotal = 0;
+    gameTotal = 0;
+}
+
+void play::roll(bool playTurn) {
+    dieValue = rand() % 6 + 1;
+    if (playTurn) {
+        cout << "You rolled a " << dieValue << ". your turn score is now " << turnTotal << endl;
+    } else {
+        cout << "Computer rolled a " << dieValue << ". They now have a turn score of " << turnTotal << endl;
+    }
+    if (dieValue == 1) {
+        turnTotal = 0;
+        if (playTurn) {
+            cout << "Sorry: you rolled a one. Your score will stay at " << gameTotal << endl;
+        } else {
+            cout << "Computer rolled a one. Their score will remain at " << gameTotal << endl << endl;
+        }
+    } else {
+        turnTotal += dieValue;
+    }
+}
+
+void play::stop(bool playTurn) {
+    gameTotal += turnTotal;
+    if (playTurn) {
+        cout << "Good choice. Your score is now " << gameTotal << endl;
+    } else {
+        cout << "Computer bagged. Their score is now " << gameTotal << endl << endl;
+    }
+    turnTotal = 0;
+    dieValue = -1;
 }
